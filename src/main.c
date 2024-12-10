403b14415e32d16915c32ff8f4be5d088cda2e96
jujugoboom <jujugoboom@gmail.com>
2024-12-09 23:11:58 -0800

    zoom works relative to mouse

--- src/main.c
+++ src/main.c
@@ -43,6 +43,7 @@ I've changed the original file - jujugogoom 2024-12-01
 #define TOGGLES "Empty;Rail;Building;Station"
 
 #define GRID_SIZE 32
+#define INV_GRID_SIZE 0.03125f
 #define WORLD_SIZE 1000
 
 const int screenWidth = 680;
@@ -101,19 +102,19 @@ int main(void)
 	// Camera init
 	//----------------------------------------------------------------------------------
 	Camera2D camera = {0};
+	camera.offset = (Vector2){0, 0};
 	camera.zoom = 1.0f;
 	camera.rotation = 0.f;
 	//----------------------------------------------------------------------------------
 
 	// Texture loading
 	//----------------------------------------------------------------------------------
-	Texture2D textures[4];
-	textures[RAIL] = LoadTexture("resources/track.png");
-	textures[BUILDING] = LoadTexture("resources/building.png");
-	textures[STATION] = LoadTexture("resources/station.png");
-	GenTextureMipmaps(&textures[RAIL]);
-	GenTextureMipmaps(&textures[BUILDING]);
-	GenTextureMipmaps(&textures[STATION]);
+	Rectangle textureRects[4];
+	textureRects[RAIL] = (Rectangle){0, 0, 32, 32};
+	textureRects[BUILDING] = (Rectangle){32, 0, 32, 32};
+	textureRects[STATION] = (Rectangle){64, 0, 32, 32};
+	Texture2D texture = LoadTexture("resources/atlas.png");
+	GenTextureMipmaps(&texture);
 	//----------------------------------------------------------------------------------
 
 	// layout_name: controls initialization
@@ -135,7 +136,7 @@ int main(void)
 		const int currScreenWidth = GetScreenWidth();
 		const int currScreenHeight = GetScreenHeight();
 		// Update gui poisition
-		const Rectangle GuiDropdownBounds = (Rectangle){(currScreenWidth / 2) - 40, 10, 80, 24};
+		const Rectangle GuiDropdownBounds = (Rectangle){(int)(currScreenWidth * .5) - 40, 10, 80, 24};
 		const Rectangle GuiDebugToggleBounds = (Rectangle){10, currScreenHeight - 30, 20, 20};
 
 		const Rectangle GuiBounds[] = {GuiDropdownBounds, GuiDebugToggleBounds};
@@ -144,21 +145,16 @@ int main(void)
 		if (wheel != 0)
 		{
 			// Get the world point that is under the mouse
-			// Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), camera);
-
-			// Set the offset to where the mouse is
-			// camera.offset = Vector2Clamp(GetMousePosition(), Vector2Zero(), Vector2Subtract(TotalSizeVector, (Vector2){GetScreenWidth(), GetScreenHeight()}));
-
-			// Set the target to match, so that the camera maps the world space point
-			// under the cursor to the screen space point under the cursor at any zoom
-			// camera.target = Vector2Clamp(mouseWorldPos, Vector2Zero(), Vector2Subtract(TotalSizeVector, (Vector2){GetScreenWidth(), GetScreenHeight()}));
+			Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), camera);
 
 			// Zoom increment
 			float scaleFactor = 1.0f + (0.25f * fabsf(wheel));
 			if (wheel < 0)
 				scaleFactor = 1.0f / scaleFactor;
 			camera.zoom = Clamp(camera.zoom * scaleFactor, 0.125f, 64.0f);
-			camera.target = Vector2Clamp(camera.target, ZeroVector, Vector2Subtract(TotalSizeVector, (Vector2){currScreenWidth / camera.zoom, currScreenHeight / camera.zoom}));
+
+			// Move camera, center around mouse position scaled by camera zoom
+			camera.target = Vector2Clamp(Vector2Subtract(mouseWorldPos, Vector2Scale(GetMousePosition(), 1.f / camera.zoom)), ZeroVector, Vector2Subtract(TotalSizeVector, (Vector2){currScreenWidth / camera.zoom, currScreenHeight / camera.zoom}));
 		}
 
 		if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT))
@@ -171,7 +167,7 @@ int main(void)
 		if (IsMouseButtonDown(MOUSE_BUTTON_LEFT) && !CheckGuiCollision(GetMousePosition(), GuiBounds, 2) && !DropdownActive)
 		{
 			Vector2 mousePos = GetScreenToWorld2D(GetMousePosition(), camera);
-			Vector2 clicked = Vector2Clamp((Vector2){mousePos.x / GRID_SIZE, mousePos.y / GRID_SIZE}, ZeroVector, WorldSizeVector);
+			Vector2 clicked = Vector2Clamp(Vector2Scale((Vector2){mousePos.x, mousePos.y}, INV_GRID_SIZE), ZeroVector, WorldSizeVector);
 			world[(int)clicked.x][(int)clicked.y] = SelectedMode;
 		}
 
@@ -187,8 +183,8 @@ int main(void)
 		// Calculate screen bounds to world bounds
 		Vector2 start = GetScreenToWorld2D(ZeroVector, camera);
 		Vector2 end = GetScreenToWorld2D((Vector2){currScreenWidth, currScreenHeight}, camera);
-		Vector2 worldStart = Vector2Clamp(Vector2Divide(start, (Vector2){GRID_SIZE, GRID_SIZE}), ZeroVector, WorldSizeVector);
-		Vector2 worldEnd = Vector2Clamp(Vector2Divide(end, (Vector2){GRID_SIZE, GRID_SIZE}), ZeroVector, WorldSizeVector);
+		Vector2 worldStart = Vector2Clamp(Vector2Scale(start, INV_GRID_SIZE), ZeroVector, WorldSizeVector);
+		Vector2 worldEnd = Vector2Clamp(Vector2Scale(end, INV_GRID_SIZE), ZeroVector, WorldSizeVector);
 
 		// Only bother rendering parts of the world on screen
 		for (int i = worldStart.x; i < worldEnd.x; i++)
@@ -203,7 +199,7 @@ int main(void)
 				if (world[i][j] != 0)
 				{
 					int currPos = world[i][j];
-					DrawTexture(textures[currPos], i * GRID_SIZE, j * GRID_SIZE, WHITE);
+					DrawTextureRec(texture, textureRects[currPos], (Vector2){i * GRID_SIZE, j * GRID_SIZE}, WHITE);
 				}
 			}
 		}
@@ -222,6 +218,11 @@ int main(void)
 			DrawText(fpsText, currScreenWidth - (MeasureText(fpsText, 20) + 20), currScreenHeight - 30, 20, GREEN);
 			const char *renderInfo = TextFormat("Rendering from x %d to %d; y %d to %d", (int)worldStart.x, (int)worldEnd.x, (int)worldStart.y, (int)worldEnd.y);
 			DrawText(renderInfo, currScreenWidth - (MeasureText(renderInfo, 20) + 20), currScreenHeight - 60, 20, GREEN);
+			const char *positionInfo = TextFormat("Currently targeting %d, %d", (int)camera.target.x, (int)camera.target.y);
+			DrawText(positionInfo, currScreenWidth - (MeasureText(positionInfo, 20) + 20), currScreenHeight - 90, 20, GREEN);
+			const Vector2 mousePos = Vector2Scale(GetMousePosition(), 1.f / camera.zoom);
+			const char *mouseInfo = TextFormat("Mouse targeting %d, %d", (int)mousePos.x, (int)mousePos.y);
+			DrawText(mouseInfo, currScreenWidth - (MeasureText(mouseInfo, 20) + 20), currScreenHeight - 120, 20, GREEN);
 		}
 		//----------------------------------------------------------------------------------
 
@@ -230,11 +231,7 @@ int main(void)
 	}
 
 	// De-Initialization
-	//--------------------------------------------------------------------------------------
-	for (int i = 1; i < 4; i++)
-	{
-		UnloadTexture(textures[i]);
-	}
+	UnloadTexture(texture);
 	CloseWindow(); // Close window and OpenGL context
 	//--------------------------------------------------------------------------------------
 
